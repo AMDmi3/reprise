857afff2c0c88dee9cdba94d5116fe97a56366d0
Dmitry Marakasov <amdmi3@amdmi3.ru>
2022-04-04 18:38:02 +0300

    Add arguments
    - Allow to specity ports and distfiles directories
    - Allow DISTDIR autodetection

--- porttester/__main__.py
+++ porttester/__main__.py
@@ -15,11 +15,14 @@
 # You should have received a copy of the GNU General Public License
 # along with portester.  If not, see <http://www.gnu.org/licenses/>.
 
+import argparse
 import asyncio
 import logging
+import os
 import signal
 from pathlib import Path
 
+from porttester.execute import execute
 from porttester.jail import start_jail
 from porttester.jail.populate import JailSpec, populate_jail
 from porttester.mount.filesystems import mount_devfs, mount_nullfs, mount_tmpfs
@@ -38,12 +41,6 @@ _JAIL_SPECS = {
 _USE_JAILS = ['13-amd64']
 
 
-_PORTSTREE = '/usr/ports'
-
-
-_DISTFILES = '/usr/distfiles'
-
-
 _PORT = 'www/py-yarl'
 
 
@@ -56,13 +53,17 @@ def replace_in_file(path: Path, pattern: str, replacement: str) -> None:
 
 
 class PortTester:
-    workdir: Workdir
+    _workdir: Workdir
+    _portsdir: Path
+    _distfilesdir: Path
 
-    def __init__(self, workdir: Workdir) -> None:
-        self.workdir = workdir
+    def __init__(self, workdir: Workdir, portsdir: Path, distfilesdir: Path) -> None:
+        self._workdir = workdir
+        self._portsdir = portsdir
+        self._distfilesdir = distfilesdir
 
     async def _get_prepared_jail(self, name: str) -> ZFS:
-        jail = self.workdir.get_jail_master(name)
+        jail = self._workdir.get_jail_master(name)
         spec = _JAIL_SPECS[name]
 
         if await jail.exists() and not (jail.get_path() / 'usr').exists():
@@ -93,9 +94,9 @@ class PortTester:
 
             instance_name = f'{jail_name}-default'
 
-            instance_zfs = self.workdir.get_jail_instance(instance_name)
+            instance_zfs = self._workdir.get_jail_instance(instance_name)
 
-            packages_zfs = self.workdir.get_jail_packages(jail_name)
+            packages_zfs = self._workdir.get_jail_packages(jail_name)
 
             if not await packages_zfs.exists():
                 await packages_zfs.create(parents=True)
@@ -125,8 +126,8 @@ class PortTester:
                 logging.debug('mounting filesystems')
                 await asyncio.gather(
                     mount_devfs(instance_zfs.get_path() / 'dev'),
-                    mount_nullfs(Path(_PORTSTREE), ports_path),
-                    mount_nullfs(Path(_DISTFILES), distfiles_path, readonly=False),
+                    mount_nullfs(self._portsdir, ports_path),
+                    mount_nullfs(self._distfilesdir, distfiles_path, readonly=False),
                     mount_nullfs(packages_zfs.get_path(), packages_path, readonly=False),
                     mount_tmpfs(work_path),
                 )
@@ -215,13 +216,42 @@ def sig_handler() -> None:
     print('Interrupted')
 
 
+async def parse_arguments() -> argparse.Namespace:
+    AUTODETECT = 'autodect'
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--ports', default='/usr/ports', metavar='PATH', help='ports tree directory to use in jails')
+    parser.add_argument('--distfiles', default=AUTODETECT, metavar='PATH', help='distfiles directory tree to use in jails')
+
+    args = parser.parse_args()
+
+    if args.distfiles == AUTODETECT:
+        distdir = await execute('make', '-C', args.ports, '-V', 'DISTDIR')
+
+        if distdir and distdir[0] and os.path.exists(distdir[0]):
+            args.distfiles = distdir[0]
+        else:
+            raise RuntimeError('cannot autodetect distfiles location')
+
+    return args
+
+
 async def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
+
+    args = await parse_arguments()
 
     asyncio.get_running_loop().add_signal_handler(signal.SIGINT, sig_handler)
 
     workdir = await Workdir.initialize()
-    await PortTester(workdir).run()
+
+    porttester = PortTester(
+        workdir=workdir,
+        portsdir=args.ports,
+        distfilesdir=args.distfiles
+    )
+
+    await porttester.run()
 
 
 asyncio.run(main())
